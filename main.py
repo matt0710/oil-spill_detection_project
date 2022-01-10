a3f2f233c21c3ce18c9b6085c4b0161bc6f81939
matteo <matteo.attimonelli1999@gmail.com>
2022-01-10 11:31:14 +0100

    quit option enabled

--- main.py
+++ main.py
@@ -14,16 +14,23 @@ if model_name == 'yolov4':
     my_yolov4 = yolov4.create_model()
     for image in images:
         yolov4.run_model(my_yolov4, str(os.getcwd()) + '\images\\test_images\\' + image, str(os.getcwd()) + '\images\\ground_truth\\' + image)
+        if 0xFF == ord('q'):
+            break
+
 
 elif model_name == 'yolov5s':
     my_yolov5 = yolov5.create_model()
     for image in images:
         yolov5.run_model(my_yolov5, str(os.getcwd()) + '\images\\test_images\\' + image, str(os.getcwd()) + '\images\\ground_truth\\' + image)
+        if 0xFF == ord('q'):
+            break
 
 elif model_name == 'yolov5s6':
     my_yolov5s6 = yolov5s6.create_model()
     for image in images:
         yolov5s6.run_model(my_yolov5s6, str(os.getcwd()) + '\images\\test_images\\' + image, str(os.getcwd()) + '\images\\ground_truth\\' + image)
+        if 0xFF == ord('q'):
+            break
 
 else:
     print("Usage error: the implemented models are <yolov4> <yolov5s> <yolov5s6>")
